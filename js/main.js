9864b1e38561023d58763c66200116b9c89b35de
colekins <colekins@gmail.com>
2019-01-27 18:09:47 -0500

    Add alternating tape image for mobile users

--- js/main.js
+++ js/main.js
@@ -12,3 +12,39 @@ function showInfo() {
         scrollTop: $(".info-box").offset().top},
         'slow');
 }
+
+var images = [], x = -1;
+images[0] = "https://i.imgur.com/TT3rBU4.png";
+images[1] = "https://f4.bcbits.com/img/a2453144117_10.jpg";
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+}
+function displayPreviousImage() {
+  x = (x <= 0) ? images.length - 1 : x - 1;
+  document.getElementById("img").src = images[x];
+}
+function startTimer() {
+    setInterval(displayNextImage, 3000);
+}
+
+function isMobile() {
+  if(
+    navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+) {
+  console.log('hi')
+  $('#buy').attr("onMouseOver", "null")
+  $('#buy').attr("onMouseOut", "null")
+  startTimer()
+  }
+}
+
+$('document').ready(function(){
+  isMobile()
+});

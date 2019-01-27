896913db0f3fce4c118a8b2911c36a7180874472
colekins <colekins@gmail.com>
2019-01-27 11:30:59 -0500

    Change bg color, increase icon size

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// function hover(element) {
-//   element.setAttribute('src', 'https://i.imgur.com/TT3rBU4.png');
-// }
-//
-// function unhover(element) {
-//   element.setAttribute('src', 'https://f4.bcbits.com/img/a2453144117_10.jpg');
-// }
-
 function hover() {
   console.log('yo')
   $("#img").attr('src', 'https://i.imgur.com/TT3rBU4.png');
